56d6e7434443f2235a3647dceace65e2b9cb3009
p25marti <paulmartins4242@gmail.com>
2018-07-02 15:29:39 -0400

    change default text editor to termite

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init(awful.util.getdir("config") .. "themes/powerarrow-dark/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "gnome-terminal"
+terminal = "termite"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
